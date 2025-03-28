708d2485262ef7fa11cc1fb1a5eb2ce16e09aa44
nadgire <abhinadgire@hotmail.com>
2025-03-29 02:22:56 +0530

    .

--- routes/users.js
+++ routes/users.js
@@ -62,7 +62,7 @@ router.post('/login', async (req, res) => {
         return res.send({ message: "User not found" })
     }
 
-    if(existingUser.isAccountActive == false){
+    if (existingUser.isAccountActive == false){
         return res.send({ message: "Account is not active. Follow 'Forgot Password' to activate the account" })
     }
 
@@ -176,7 +176,7 @@ router.post('/register-otp', async (req, res) => {
         from: process.env.email,
         to: obj.email,
         subject: `ToDo Registration OTP - ${otp}`,
-        text: `Your OTP for registration on ToDo Manager is: ${otp}. OTP is valid for next 5 mins.`,
+        text: `Your OTP for registration on ToDo Manager is: '${otp}'. OTP is valid for next 5 mins.`,
     };
 
     try {
@@ -198,11 +198,73 @@ router.put('/delete-account', async (req, res) => {
         const user = await User.findOneAndUpdate({ email: obj.email }, { $set: { 'isAccountActive': false } });
         return res.send({ message: "Account deleted successfully" });
 
-    }    
+    }
     else {
         return res.send({ message: "Account deletion failed" });
     }
-})
+});
 
+router.put('/forgot-pass-otp', async (req, res) => {
+    const obj = req.body;
+    console.log(obj)
+
+    const existingUser = await User.findOne({ email: obj.email });
+
+    if (existingUser == null || existingUser == undefined) {
+        return res.send({ message: 'User is invalid' });
+
+    }
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    const expirationTime = Date.now() + 5 * 60 * 1000;
+    otpStore[obj.email] = { otp, expirationTime };
+
+    const mailOptions = {
+        from: process.env.email,
+        to: obj.email,
+        subject: `ToDo Forgot Password OTP - ${otp}`,
+        text: `Your OTP for resetting the forgotten password on ToDo Manager is: '${otp}'. OTP is valid for next 5 mins.`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        return res.send({ message: 'OTP sent successfully' });
+    } catch (error) {
+        console.error('Error sending OTP:', error);
+        return res.send({ error: 'Failed to send OTP' });
+    }
+});
+
+router.post('/confirm-otp', async (req, res) => {
+    const obj = req.body;
+    console.log(obj);
+
+    if (!otpStore[obj.email]) {
+        return res.send({ error: 'OTP not found for this email' });
+    }
+    const storedOtp = otpStore[obj.email];
+    if (Date.now() > storedOtp.expirationTime) {
+        delete otpStore[obj.email];
+        return res.send({ error: 'OTP has expired. Please reset.' });
+    }
+    if (storedOtp.otp !== parseInt(obj.otp)) {
+        return res.send({ error: 'Invalid OTP' });
+    }
+    delete otpStore[obj.email];
+
+    return res.send({ message: 'OTP matched' });
+});
+
+
+router.post('/reset-password', async (req, res) => {
+    const obj = req.body;
+    console.log(obj);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(obj.password, salt);
+    const user = await User.updateOne({ email: obj.email }, { $set: { 'password': hashedPassword, 'isAccountActive': true } });
+    console.log(user);
+    return res.send({ message: "Password reset successful" })
+});
 
 module.exports = router;
